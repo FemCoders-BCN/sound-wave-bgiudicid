0777d53b57edfcee2b9ad4b9b7f92c3f84e72166
Bianca Giudici <b.giudicid@gmail.com>
2023-01-09 14:36:02 -0800

    styles circles2

--- src/components/BodyOne.jsx
+++ src/components/BodyOne.jsx
@@ -3,9 +3,14 @@ import '../styles/SoundWave.css'
 
 export default function BodyOne(){
     return(
+        <>
+        <div>
+            <div className="circle1"></div>
+        </div>
         <div className="body">
             <h1>Feel The Music</h1> 
             <h3>Stream over 20 thousand songs with one click</h3>
         </div>
+    </>
     )
 }
--- src/pages/SoundWave.jsx
+++ src/pages/SoundWave.jsx
@@ -2,20 +2,24 @@ import Nav from "../components/Nav"
 //import Button from "../components/Button"
 import BodyOne from "../components/BodyOne"
 import { Outlet, Link } from "react-router-dom";
-import Circles from "../components/Circles"
 
 export default function Home(){
     return(
         <>
+        <div>
+            <Nav/>   
+            <BodyOne/>
+            <Link className="join-now" to ="/join">Join Now</Link>
             <div>
-                <Nav/> 
-                <Circles/>  
-                <BodyOne/>
-                <Link className="join-now" to ="/join">Join Now</Link>
+                <div className="circle2"></div>
             </div>
+            <div>
+                <div className="circle3"></div>
+            </div>
+        </div>
 
-            <Outlet/>
-        </>
-    )
+        <Outlet/>
+    </>
+)
 }
 

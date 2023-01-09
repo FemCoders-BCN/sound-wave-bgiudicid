c78e6bd7187b8113e9032ca619a172c46a23e264
Bianca Giudici <b.giudicid@gmail.com>
2023-01-09 12:22:38 -0800

    copied from original

---
+++ src/components/BodyOne.jsx
@@ -0,0 +1,11 @@
+import '../styles/Nav.css';
+import '../styles/SoundWave.css'
+
+export default function BodyOne(){
+    return(
+        <div className="body">
+            <h1>Feel The Music</h1> 
+            <h3>Stream over 20 thousand songs with one click</h3>
+        </div>
+    )
+}
